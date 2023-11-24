f38426d5ff1fccd9c02456b63793f67f4f5b9926
ChangyungKim <ghkduack@gmail.com>
2023-11-25 02:14:05 +0900

    모든 test case 3개씩 완성
    오류 없음 확인

--- HolubSQL/src/com/holub/database/XMLExporterTest.java
+++ HolubSQL/src/com/holub/database/XMLExporterTest.java
@@ -17,11 +17,11 @@ public class XMLExporterTest {
         people.insert(new String[]{"Ichabod", "Crane", "2"});
         people.insert(new String[]{"Rip", "VanWinkle", "3"});
         people.insert(new String[]{"Goldie", "Locks", "4"});
-        Writer out = new FileWriter("c:/dp2023/people");
+        Writer out = new FileWriter("c:/dp2023/people.xml");
         XMLExporter builder1 = new XMLExporter(out);
         people.export(builder1);
         out.close();
-        File file = new File("c:/dp2023/people");
+        File file = new File("c:/dp2023/people.xml");
         StringBuffer stringBuffer = new StringBuffer();
         FileReader fileReader = new FileReader(file);
         int index = 0;
@@ -34,34 +34,20 @@ public class XMLExporterTest {
         
         fileReader.close();
         System.out.println("------------------------------------------");
-        /*
-
-        StringBuffer stringBuffer1 = new StringBuffer();
-        File file1 = new File("c:/dp2020/people.xml");
-        FileReader fileReader1 = new FileReader(file1);
-        int index1 = 0;
-        while ((index1 = fileReader1.read()) != -1) {
-            stringBuffer1.append((char) index1);
-
-        }
-//        getFileVisitor테스트
-        assertEquals(stringBuffer.toString(), is(equalTo("<root><title>people</title><DATA><First>Allen</First><Last>Holub</Last><Id>1</Id></DATA><DATA><First>Ichabod</First><Last>Crane</Last>" +
-                "<Id>2</Id></DATA><DATA><First>Rip</First><Last>VanWinkle</Last><Id>3</Id></DATA><DATA><First>Goldie</First><Last>Locks</Last><Id>4</Id></DATA></root>")));
-        fileReader.close();
-        fileReader1.close();
-
-        System.out.println("------------------------------------------");
-
-        Table university = TableFactory.create("university",
-                new String[]{"name", "location",});
-        university.insert(new String[]{"chungang", "seoul"});
-        university.insert(new String[]{"seoul", "seoul"});
-        university.insert(new String[]{"woosong", "daejeon"});
-        Writer out1 = new FileWriter("c:/dp2020/university");
+        
+        Table bloodstorage = TableFactory.create("bloodstorage",
+                new String[]{"Id", "Name", "Bloodtype", "Age", "Gender"});
+        bloodstorage.insert(new String[]{"1", "Allen", "A", "20", "F"});
+        bloodstorage.insert(new String[]{"2", "Troy", "B", "25", "M"});
+        bloodstorage.insert(new String[]{"3", "Kevin", "AB", "40", "M"});
+        bloodstorage.insert(new String[]{"4", "Goldie", "O", "30", "F"});
+        bloodstorage.insert(new String[]{"5", "Chunsik", "B", "31", "M"});
+        bloodstorage.insert(new String[]{"6", "Bob", "A", "27", "M"});
+        Writer out1 = new FileWriter("c:/dp2023/bloodstorage.xml");
         XMLExporter builder2 = new XMLExporter(out1);
-        university.export(builder2);
+        bloodstorage.export(builder2);
         out1.close();
-        File file2 = new File("c:/dp2020/university");
+        File file2 = new File("c:/dp2023/bloodstorage.xml");
         StringBuffer stringBuffer2 = new StringBuffer();
         FileReader fileReader2 = new FileReader(file2);
         int index2 = 0;
@@ -69,24 +55,36 @@ public class XMLExporterTest {
             stringBuffer2.append((char) index2);
 
         }
-//        XMLExporter테스트
-        assertThat(stringBuffer2.toString(), is(equalTo("<root><title>university</title><DATA><name>chungang</name><location>seoul</location></DATA><DATA><name>seoul</name><location>seoul</location>" +
-                "</DATA><DATA><name>woosong</name><location>daejeon</location></DATA></root>")));
-        builder2.accept(new getFileVisitor("university", university));
-
+        
+        assertEquals(stringBuffer2.toString(), "<?xml  version=\"1.0\"  encoding=\"UTF-8\" ?><Table><TableName>bloodstorage</TableName><Data><Id>1</Id><Name>Allen</Name><Bloodtype>A</Bloodtype><Age>20</Age><Gender>F</Gender></Data><Data><Id>2</Id><Name>Troy</Name><Bloodtype>B</Bloodtype><Age>25</Age><Gender>M</Gender></Data><Data><Id>3</Id><Name>Kevin</Name><Bloodtype>AB</Bloodtype><Age>40</Age><Gender>M</Gender></Data><Data><Id>4</Id><Name>Goldie</Name><Bloodtype>O</Bloodtype><Age>30</Age><Gender>F</Gender></Data><Data><Id>5</Id><Name>Chunsik</Name><Bloodtype>B</Bloodtype><Age>31</Age><Gender>M</Gender></Data><Data><Id>6</Id><Name>Bob</Name><Bloodtype>A</Bloodtype><Age>27</Age><Gender>M</Gender></Data></Table>");
+        
+        fileReader2.close();
+        System.out.println("------------------------------------------");
+        
+        Table movie = TableFactory.create("movie",
+                new String[]{"Title", "Releaseyear", "Runningtime", "Rating", "Genre"});
+        movie.insert(new String[]{"The Shawshank Redemption", "1994", "142m", "9.3", "Drama"});
+        movie.insert(new String[]{"The Godfather", "1972", "175m", "9.2", "Crime"});
+        movie.insert(new String[]{"The Dark Knight", "2008", "152m", "9.0", "Action"});
+        movie.insert(new String[]{"Forrest Gump", "1994", "142m", "8.8", "Drama"});
+        movie.insert(new String[]{"Oppenheimer", "2023", "180m", "8.5", "Biography"});
+        Writer out2 = new FileWriter("c:/dp2023/movie.xml");
+        XMLExporter builder3 = new XMLExporter(out2);
+        movie.export(builder3);
+        out2.close();
+        File file3 = new File("c:/dp2023/movie.xml");
         StringBuffer stringBuffer3 = new StringBuffer();
-        File file3 = new File("c:/dp2020/university.xml");
         FileReader fileReader3 = new FileReader(file3);
         int index3 = 0;
         while ((index3 = fileReader3.read()) != -1) {
             stringBuffer3.append((char) index3);
 
         }
-//        getFileVisitor테스트
-        assertThat(stringBuffer3.toString(), is(equalTo("<root><title>university</title><DATA><name>chungang</name><location>seoul</location></DATA><DATA><name>seoul</name><location>seoul</location>" +
-                "</DATA><DATA><name>woosong</name><location>daejeon</location></DATA></root>")));
-        fileReader2.close();
+//        HTMLExporter테스트
+        assertEquals(stringBuffer3.toString(), "<?xml  version=\"1.0\"  encoding=\"UTF-8\" ?><Table><TableName>movie</TableName><Data><Title>The Shawshank Redemption</Title><Releaseyear>1994</Releaseyear><Runningtime>142m</Runningtime><Rating>9.3</Rating><Genre>Drama</Genre></Data><Data><Title>The Godfather</Title><Releaseyear>1972</Releaseyear><Runningtime>175m</Runningtime><Rating>9.2</Rating><Genre>Crime</Genre></Data><Data><Title>The Dark Knight</Title><Releaseyear>2008</Releaseyear><Runningtime>152m</Runningtime><Rating>9.0</Rating><Genre>Action</Genre></Data><Data><Title>Forrest Gump</Title><Releaseyear>1994</Releaseyear><Runningtime>142m</Runningtime><Rating>8.8</Rating><Genre>Drama</Genre></Data><Data><Title>Oppenheimer</Title><Releaseyear>2023</Releaseyear><Runningtime>180m</Runningtime><Rating>8.5</Rating><Genre>Biography</Genre></Data></Table>");
+        
         fileReader3.close();
-    }*/
+        System.out.println("------------------------------------------");
+       
     }
 }
--- HolubSQL/src/com/holub/database/XMLImporterTest.java
+++ HolubSQL/src/com/holub/database/XMLImporterTest.java
@@ -22,7 +22,7 @@ public class XMLImporterTest { //반드시 XMLExporterTest진행후 xml형식의
     @Test
     public void XMLImporter() throws IOException, ParserConfigurationException, SAXException {
        
-        String file = "c:/dp2023/people"; //XMLExporter로 내보낸 people을 읽어보겠다.
+        String file = "c:/dp2023/people.xml"; //XMLExporter로 내보낸 people을 읽어보겠다.
         FileReader fr=new FileReader(file);
         XMLImporter builder1 = new XMLImporter(fr);
         builder1.startTable();
@@ -42,22 +42,46 @@ public class XMLImporterTest { //반드시 XMLExporterTest진행후 xml형식의
            i++;
         }
         
-        
-        
-        
-        //assertEquals(expectedIterator, row);
-        
-        /*String file2 = "c:/dp2020/university"; //XMLExporter로 내보낸 university을 읽어보겠다.
-        XMLImporter builder2 = new XMLImporter(file2);
+        String file2="c:/dp2023/bloodstorage.xml";
+        FileReader fr2=new FileReader(file2);
+        XMLImporter builder2=new XMLImporter(fr2);
         builder2.startTable();
-        assertEquals(builder2.loadTableName(), is(equalTo("university")));
-        assertEquals(builder2.loadWidth(), 2);
+        assertEquals(builder2.loadTableName(), "bloodstorage");
+        assertEquals(builder2.loadWidth(), 5);
         Iterator row2;
-        row2 = builder2.loadRow();
-        String[][] expected2 = {{"chungang", "seoul"}, {"seoul", "seoul"}, {"woosong", "daejeon"}};
-        assertEquals(expected2, row2);
-      */
-
+        row2=builder2.loadRow();
+        String[][] expected2= {{"1", "Allen", "A", "20", "F"}, {"2", "Troy", "B", "25", "M"}, {"3", "Kevin", "AB", "40", "M"}, {"4", "Goldie", "O", "30", "F"}, {"5", "Chunsik", "B", "31", "M"}, {"6", "Bob", "A", "27", "M"}};
+        Iterator<String> expected2Iterator=convertArrayToIterator(expected2);
+        
+        i=0;
+        while(expected2Iterator.hasNext()) {
+        	if(i!=0&&i%5==0) {
+        		row2=builder2.loadRow();
+        	}
+        	assertEquals(expected2Iterator.next(), row2.next());
+        	i++;
+        }
+        
+        String file3="c:/dp2023/movie.xml";
+        FileReader fr3=new FileReader(file3);
+        XMLImporter builder3=new XMLImporter(fr3);
+        builder3.startTable();
+        assertEquals(builder3.loadTableName(), "movie");
+        assertEquals(builder3.loadWidth(), 5);
+        Iterator row3;
+        row3=builder3.loadRow();
+        String[][] expected3= {{"The Shawshank Redemption", "1994", "142m", "9.3", "Drama"}, {"The Godfather", "1972", "175m", "9.2", "Crime"}, {"The Dark Knight", "2008", "152m", "9.0", "Action"}, {"Forrest Gump", "1994", "142m", "8.8", "Drama"}, {"Oppenheimer", "2023", "180m", "8.5", "Biography"}};
+        Iterator<String> expected3Iterator=convertArrayToIterator(expected3);
+        
+        i=0;
+        while(expected3Iterator.hasNext()) {
+        	if(i!=0&&i%5==0) {
+        		row3=builder3.loadRow();
+        	}
+        	assertEquals(expected3Iterator.next(), row3.next());
+        	i++;
+        }
+       	
     }
 
 private static Iterator<String> convertArrayToIterator(String[][] array) {
