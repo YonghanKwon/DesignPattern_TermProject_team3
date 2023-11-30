89633c6b9c5fc05d42e163dcf2023bb1ddc85e0a
YonghanKwon <kt3155@naver.com>
2023-11-30 19:10:27 +0900

    change update

---
+++ HolubSQL/src/DP_Application/viewRecord.java
@@ -0,0 +1,13 @@
+package DP_Application;
+
+import java.io.IOException;
+
+import com.holub.database.Table;
+import com.holub.database.TableFactory;
+
+public class viewRecord {
+
+	public viewRecord() throws IOException {
+
+	}
+}
